a5be1279f661f62d3b379691a8223896ef991c4d
Passakorn <0658562149za@gmail.com>
2026-07-16 16:59:07 +0700

    update

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -9,32 +9,34 @@ class RolePolicy
 {
     public function viewAny(User $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user->hasRole(Role::ADMIN);
     }
 
     public function view(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin');
+        return $user->hasRole(Role::ADMIN);
     }
 
     public function create(User $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user->hasRole(Role::ADMIN);
     }
 
     public function update(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin');
+        return $user->hasRole(Role::ADMIN);
     }
 
     public function delete(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin');
+        return $user->hasRole(Role::ADMIN);
     }
 
     public function export(User $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user->hasRole(Role::ADMIN);
     }
 }
+
+
 
